4dffcc1913c0e4801dde192907c1c567383c2479
1E10 Laptop <sample@bths.edu>
2023-03-07 10:28:13 -0500

    stuff

--- src/Computer.java
+++ src/Computer.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Computer {
+public class Computer {
+    private int screenSize;
+    private int memory;
+
+    public Computer(int screenSize, int memory) {
+        this.memory = memory;
+        this.screenSize = screenSize;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public int getScreenSize() {
+        return screenSize;
+    }
+
+    public void installOperatingSystem() {
+        System.out.println("installing OS...");
+    }
 }
